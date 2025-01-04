946be283ca2e55cea2c0b1a3c81fa1cdcda632ee
Wesley Hershberger <wesley.hershberger@gmail.com>
2025-01-04 08:58:41 -0600

    Fix signed commit parsing
    `git cat-file -p` prints one empty line in the gpgsig field. `string.match`
    then returns null for that line, causing an exception during pattern matching.

--- src/string_utils.js
+++ src/string_utils.js
@@ -3,6 +3,6 @@ const angles = exp => `<${exp}>`;
 const quote = exp => `"${exp}"`;
 const head8 = str => str.substr(0, 8);
 const splitLines = string => string.split("\n");
-const splitByWhitespace = string => string.match(/\S+/g);
+const splitByWhitespace = string => string.split(/\s+/);
 
 module.exports = { brackets, angles, quote, head8, splitLines, splitByWhitespace };
